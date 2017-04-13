7f63ee355552d70723f8eee2aae1d73c282fe507
Forrest Filler <forrestfiller@users.noreply.github.com>
2017-04-12 21:35:02 -0400

    resource tidy

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 router.get('/:resource', function(req, res, next) {
 	var resource = req.params.resource
-	
+
 		res.json({
 			confirmation: 'success',
-			message: resource
+			resource: resource
 		})
 
 
