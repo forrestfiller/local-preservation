ea4c3edcf045af2d8261dd82cc580e2314fd87ce
Forrest Filler <forrestfiller@users.noreply.github.com>
2017-04-22 17:01:11 -0400

    setting up another api route for menu option

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,14 @@ var router = express.Router();
 
 router.get('/:resource', function(req, res, next) {
 	var resource = req.params.resource
+	if(resource == 'starwars'){
+		console.log('nam?  ')
+
 		res.json({
 			confirmation: 'success',
 			resource: resource
-		})
+		});
+	};
 });
 
 module.exports = router;
