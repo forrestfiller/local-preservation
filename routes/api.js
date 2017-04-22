e51787076166d31d24d948d0a61f366eb039ef66
Forrest Filler <forrestfiller@users.noreply.github.com>
2017-04-22 17:53:38 -0400

    styling and setup basic meet nam, nam page

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,15 @@ var router = express.Router();
 
 router.get('/:resource', function(req, res, next) {
 	var resource = req.params.resource
-	if(resource == 'starwars'){
-		console.log('nam?  ')
 
-		res.json({
-			confirmation: 'success',
-			resource: resource
-		});
+	if(resource == 'nam-wayne-storm-trooper-suit'){
+		// console.log('nam?  ')
+		// res.json({
+		// 	confirmation: 'success',
+		// 	resource: resource
+		// });
+		res.render('meet-nam')
+
 	};
 });
 
